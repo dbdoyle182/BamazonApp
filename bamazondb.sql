b3c6c84a12e9ca3855c5a99f3ec96658ef1edff8
Daniel Doyle <dbdoyle182@gmail.com>
2018-03-27 21:49:30 -0400

    added seeds

--- bamazondb.sql
+++ bamazondb.sql
@@ -13,18 +13,7 @@ stock_quantity INT (10),
 PRIMARY KEY(item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Supersonic Headphones', 'Electronics', 60.99, 50),
-('Auto Kitty Litter', 'Pet Goods', 49.99, 25),
-('Mega Juicer', 'Appliances', 99.99, 40),
-('Baby Safe Car Seat', 'Baby Products', 39.50, 50),
-('Breast Pump', 'Baby Products', 30.00, 65),
-('Orbital Sander', 'Home Improvement', 65.00, 20),
-('Ultra Allergy Medicine', 'Medicine', 24.99, 100),
-('Star Wars Lightsaber', 'Toys and Games', 109.99, 15),
-('Poop Emoji Pillow', 'Home Decor', 14.99, 500),
-('Cat Leash', 'Pet Goods', 11.99, 35),
-('Cold Brew Maker', 'Appliances', 34.99, 100);
+
 
 SELECT *
 FROM products;
