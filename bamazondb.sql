617383ea2ca0cb8302d0baab5d6f9fab410f16df
Daniel Doyle <dbdoyle182@gmail.com>
2018-03-28 16:04:46 -0400

    added new columns in sql

--- bamazondb.sql
+++ bamazondb.sql
@@ -10,6 +10,8 @@ product_name VARCHAR(30),
 department_name VARCHAR(30),
 price DECIMAL(10, 2),
 stock_quantity INT (10),
+item_bought INT (4) DEFAULT 0 NOT NULL,
+item_sales DECIMAL(8, 2) DEFAULT 0 NOT NULL,
 PRIMARY KEY(item_id)
 );
 
