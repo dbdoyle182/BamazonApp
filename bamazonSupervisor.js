bbec78d824da6d21178d6f21c5bdd15eacf64a58
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-04 14:46:27 -0400

    manager prompt built

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -20,7 +20,7 @@ var Supervisor = function () {
     var supervisor = this;
 
     //Place the customer constructor into the supervisor
-    this.newCustomer = function() {
+    this.newCustomer = function () {
         var newCustomer = new UserSelect();
         newCustomer.connectToDatabase();
     };
@@ -30,42 +30,78 @@ var Supervisor = function () {
         newManager.managerMenu();
     };
     // Method to display the full table and products
-    this.tableViewAll = function() {
-        connection.query('SELECT * FROM products', function(err, res) {
-            
+    this.tableViewAll = function () {
+        connection.query('SELECT * FROM products', function (err, res) {
+
             if (err) throw err;
-                var t = new Table({
-                    horizontalLine: true,
-                    width: ['10%', '30%', '10%', '10%','20%','10%','10%']
-                });
-                tableLength = res.length;
+            var t = new Table({
+                horizontalLine: true,
+                width: ['10%', '30%', '10%', '10%', '20%', '10%', '10%']
+            });
+            tableLength = res.length;
+            t.push(
+                ['ID', 'Product Name', 'Price', 'Quantity', 'Department', 'Items Bought', 'Total Sales']
+            );
+            for (var i = 0; i < tableLength; i++) {
                 t.push(
-                    ['ID', 'Product Name', 'Price', 'Quantity','Department','Items Bought','Total Sales']
-                );
-                for (var i = 0; i < tableLength; i++) {
-                    t.push(
-                        [chalk.blue(res[i].item_id), chalk.yellow(res[i].product_name), chalk.green('$' + (res[i].price).toFixed(2)), chalk.red(res[i].stock_quantity), chalk.red(res[i].department_name), chalk.red(res[i].item_bought),chalk.red(`$${res[i].item_sales.toFixed(2)}`)]
-                    )
-                };
-                console.log('Welcome to the supervisor management services')
-                console.log('' + t)
+                    [chalk.blue(res[i].item_id), chalk.yellow(res[i].product_name), chalk.green('$' + (res[i].price).toFixed(2)), chalk.red(res[i].stock_quantity), chalk.red(res[i].department_name), chalk.red(res[i].item_bought), chalk.red(`$${res[i].item_sales.toFixed(2)}`)]
+                )
+            };
+            console.log('Welcome to the supervisor management services')
+            console.log('' + t)
+            supervisor.promptSupervisor()
         });
     }
 
     // Method that allows the supervisor to see the sales by department
     this.viewDepartmentSales = function () {
-        
+
     };
 
     // Method that allows the supervisor to create a new department
-    this.createDepartment = function() {
+    this.createDepartment = function () {
         // Function logic
     };
-    
-    
-    
+
+    // Method that creates the supervisor menu
+    this.promptSupervisor = function () {
+        inquirer
+            .prompt([{
+                type: "list",
+                name: "choice",
+                message: "What would you like to do?",
+                choices: ["View All Products", "View All Departments", "View Product Sales by Department", "Create New Department", "Quit"]
+            }])
+            .then(function (answer) {
+                // Checking to see what option the user chose and running the appropriate function
+                switch (answer.choice) {
+                    case "View All Products":
+                        supervisor.tableViewAll();
+                        break;
+                    case "View All Departments":
+                        console.log(answer.choice);
+                        break;
+                    case "View Product Sales by Department":
+                        console.log(answer.choice);
+                        break;
+                    case "Create New Department":
+                        console.log(answer.choice);
+                        break;
+                    case "Quit":
+                        console.log(answer.choice);
+                        break;
+                    default:
+                        console.log("How?");
+                        break;
+                }
+            });
+    }
+
+
+
 };
-// var NewSupervisor = new Supervisor();
+var NewSupervisor = new Supervisor();
+NewSupervisor.promptSupervisor();
 // NewSupervisor.tableViewAll();
 // Exports the constructor to any files
 module.exports = Supervisor;
